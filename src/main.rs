f1cfcef79dc19b32eefce0c590cb305d358a26ae
Giorgio Pomettini <giorgio.freespire@gmail.com>
2019-06-04 19:18:50 +0200

    Added team functions

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ extern crate serde_derive;
 
 use csv::{Reader, ReaderBuilder};
 use std::collections::HashMap;
-
-use std::fs::*;
+use std::cmp::Eq;
 use std::path::Path;
+use std::fs::*;
 
 #[derive(Eq, PartialEq, Hash)]
 pub enum Skills {
@@ -18,12 +18,28 @@ pub enum Skills {
     Teamwork,
 }
 
-pub struct Team {}
+#[derive(Default)]
+pub struct Team {
+    pub students: Vec<Student>,
+}
 
 #[derive(Default)]
 pub struct Student {
     pub surname: String,
     pub skills: HashMap<Skills, u8>,
+    pub average_skill_level: f32,
+}
+
+impl Student {
+    pub fn get_average_skills(&self) -> f32 {
+        let mut sum: u8 = 0;
+
+        for skill in &self.skills {
+            sum += *&skill.1;
+        }
+
+        sum as f32 / self.skills.len() as f32
+    }
 }
 
 #[derive(Default)]
@@ -90,6 +106,31 @@ impl TeamBuilder {
 
         Ok(())
     }
+
+    fn check_number_of_teams(&self, students_per_team: usize) -> Option<(usize, usize)> {
+        if students_per_team >= self.students.len() {
+            return None;
+        }
+
+        let quotient = self.students.len() / students_per_team;
+        let remainder = self.students.len() % students_per_team;
+
+        Some((quotient, remainder))
+    }
+
+    fn calculate_teams_skill_level(&mut self) {
+        for student in &mut self.students {
+            student.average_skill_level = student.get_average_skills();
+        }
+    }
+
+    fn sort_teams_by_skill_level(&mut self) {
+        self.students.sort_by(|a, b| {
+            b.average_skill_level
+                .partial_cmp(&a.average_skill_level)
+                .unwrap()
+        });
+    }
 }
 
 fn main() {
@@ -109,6 +150,7 @@ mod tests {
     macro_rules! SETUP_TEAMBUILDER_TEST {
         ($file:expr, $path:ident, $tb:ident) => {
             let $path = Path::new($file);
+            #[allow(unused_mut)]
             let mut $tb = TeamBuilder::load_file(&$path).expect("File not found");
         };
     }
@@ -149,41 +191,59 @@ mod tests {
 
     #[test]
     fn test_get_students_number_green() {
-        unimplemented!();
+        SETUP_TEAMBUILDER_TEST_AND_INIT!(TEST_FILE, path, tb);
+        assert_eq!(tb.students.len(), 6);
     }
 
     #[test]
+    #[should_panic]
     fn test_get_students_number_red() {
-        unimplemented!();
+        SETUP_TEAMBUILDER_TEST_AND_INIT!(TEST_FILE, path, tb);
+        assert_eq!(tb.students.len(), 0);
     }
 
     #[test]
     fn test_get_students_skills_average_green() {
-        unimplemented!();
+        SETUP_TEAMBUILDER_TEST_AND_INIT!(TEST_FILE, path, tb);
+        assert_eq!(tb.students[0].get_average_skills(), 2.0);
+        assert_eq!(tb.students[1].get_average_skills(), 2.1666667);
     }
 
     #[test]
+    #[should_panic]
     fn test_get_students_skills_average_red() {
-        unimplemented!();
+        SETUP_TEAMBUILDER_TEST_AND_INIT!(TEST_FILE, path, tb);
+        assert_eq!(tb.students[0].get_average_skills(), 3.0);
+        assert_eq!(tb.students[1].get_average_skills(), 3.0);
     }
 
     #[test]
-    fn test_distribute_to_one_team() {
-        unimplemented!();
+    fn test_check_number_of_teams_divisible_first() {
+        SETUP_TEAMBUILDER_TEST_AND_INIT!(TEST_FILE, path, tb);
+        assert_eq!(tb.check_number_of_teams(2), Some((3, 0)));
     }
 
     #[test]
-    fn test_distribute_to_two_teams() {
-        unimplemented!();
+    fn test_check_number_of_teams_divisible_second() {
+        SETUP_TEAMBUILDER_TEST_AND_INIT!(TEST_FILE, path, tb);
+        assert_eq!(tb.check_number_of_teams(3), Some((2, 0)));
     }
 
     #[test]
-    fn test_distribute_to_three_teams() {
-        unimplemented!();
+    fn test_check_number_of_teams_not_divisible() {
+        SETUP_TEAMBUILDER_TEST_AND_INIT!(TEST_FILE, path, tb);
+        assert_eq!(tb.check_number_of_teams(4), Some((1, 2)));
     }
 
     #[test]
-    fn test_distribute_to_four_teams() {
-        unimplemented!();
+    fn test_check_number_of_teams_same_size() {
+        SETUP_TEAMBUILDER_TEST_AND_INIT!(TEST_FILE, path, tb);
+        assert_eq!(tb.check_number_of_teams(6), None);
+    }
+
+    #[test]
+    fn test_check_number_of_teams_exceed() {
+        SETUP_TEAMBUILDER_TEST_AND_INIT!(TEST_FILE, path, tb);
+        assert_eq!(tb.check_number_of_teams(7), None);
     }
 }
