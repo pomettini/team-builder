af12e7b3e550baf059f687de78cbc445d2c6f6f2
Giorgio Pomettini <giorgio.freespire@gmail.com>
2019-06-14 01:40:26 +0200

    Table generation is now working

--- src/html_exporter.rs
+++ src/html_exporter.rs
@@ -1,14 +1,17 @@
 use crate::builder::*;
+use std::fs::File;
+use std::io::prelude::*;
+use std::process::Command;
 
 #[allow(dead_code)]
 const HTML_HEADER: &str = "<!DOCTYPE html>
 <html>
 <head>
+<title>Team Builder</title>
 <style>
 table {
   font-family: arial, sans-serif;
   border-collapse: collapse;
-  width: 100%;
 }
 
 td, th {
@@ -29,20 +32,33 @@ const HTML_FOOTER: &str = "</body>
 </html>";
 
 // TODO: Export as an external crate
+// TODO: Add missing cells if empty
 
 pub fn generate_html(teams: &Vec<Team>) {
   let mut html = String::new();
+
+  html.push_str(HTML_HEADER);
 
   html.push_str("<table>");
 
   for team in teams {
     html.push_str("<tr>");
+
+    html.push_str(&format!("<th>{}</th>", "Team name"));
+
     for student in &team.students {
-      html.push_str(&student.surname);
+      html.push_str(&format!("<td>{}</td>", &student.surname));
     }
+
     html.push_str("</tr>");
   }
+
   html.push_str("</table>");
 
-  println!("{}", html);
+  html.push_str(HTML_FOOTER);
+
+  let mut file = File::create("table.html").unwrap();
+  file.write_all(html.as_bytes()).unwrap();
+
+  Command::new("open").arg("table.html").output().unwrap();
 }
